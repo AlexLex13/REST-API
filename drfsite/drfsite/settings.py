a749f9002913a6218af35caf7106c349deed7aca
AlexLex13 <93290744+AlexLex13@users.noreply.github.com>
2022-07-17 15:16:33 +0300

    No secrets

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xafccqjvxb*inp*j6ey7k3zd-0l&n148lthejuoo7&2wz!a&cw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
